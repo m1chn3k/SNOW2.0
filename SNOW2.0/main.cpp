7ea7f35106340dda4d91b36c376a56d6233009f4
m1chn3k <m1chn3k@gmail.com>
2026-06-12 21:36:38 +0300

    Full working code for 128 bits

--- SNOW2.0/main.cpp
+++ SNOW2.0/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <iomanip>
 #include "snow2.hpp"
 
 int main(int argc, char* argv[]) {
@@ -13,11 +15,41 @@ int main(int argc, char* argv[]) {
         0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff, 0x00
     };
 
-    Snow2 cipher;
+    std::string secret_text = "SNOW 2.0 crypto-core operational!";
 
-    std::cout << "Starting SNOW 2.0 Initialization Test...\n\n";
+    std::vector<uint8_t> input_bytes(secret_text.begin(), secret_text.end());
+    std::vector<uint8_t> encrypted_bytes;
+    std::vector<uint8_t> decrypted_bytes;
 
-    cipher.InitCipher(test_key, test_iv);
+    std::cout << "============= SNOW 2.0 SYSTEM TEST =============\n\n";
+
+    Snow2 enc_cipher;
+    enc_cipher.InitCipher(test_key, test_iv);
+    enc_cipher.ProcessBytes(input_bytes, encrypted_bytes);
+
+    Snow2 dec_cipher;
+    dec_cipher.InitCipher(test_key, test_iv);
+    dec_cipher.ProcessBytes(encrypted_bytes, decrypted_bytes);
+
+    std::cout << "Original Text:  " << secret_text << "\n\n";
+
+    std::cout << "Ciphertext (HEX): ";
+    for (uint8_t b : encrypted_bytes) {
+        std::cout << std::hex << std::setw(2) << std::setfill('0') << (int)b << " ";
+    }
+    std::cout << "\n\n";
+
+    std::string recovered_text(decrypted_bytes.begin(), decrypted_bytes.end());
+    std::cout << "Recovered Text: " << recovered_text << "\n\n";
+
+    std::cout << "------------------------------------------------\n";
+    if (secret_text == recovered_text) {
+        std::cout << "[ STATUS: SUCCESS ] Encryption/Decryption match perfectly!\n";
+    }
+    else {
+        std::cout << "[ STATUS: ERROR ] Text mismatch! Check the byte logic.\n";
+    }
+    std::cout << "================================================\n";
 
     return 0;
 }
